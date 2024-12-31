cadfd8ffce6b59689f1047bee0833d079f73411f
Dana <dana.joukhadar3@gmail.com>
2024-12-31 13:11:46 +0300

    I was sending the code to Lujain and Lujain was merging my code with hers and pushing it

--- src/main/java/org/example/model/FilePath.java
+++ src/main/java/org/example/model/FilePath.java
@@ -1,9 +1,9 @@
 package org.example.model;
 
 public abstract class FilePath {
-    private static final String menuItemsFilePath = "C:\\Users\\g\\IdeaProjects\\RestaurantCode4\\src\\main\\resources\\MenuItemsFile.txt";
-    private static final String typesAndIDs = "C:\\Users\\g\\IdeaProjects\\RestaurantCode4\\src\\main\\resources\\TypesAndIDsOfItems.txt";
-    private static final String accounts ="C:\\Users\\g\\IdeaProjects\\RestaurantCode4\\src\\main\\resources\\Accounts.txt";
+    private static final String menuItemsFilePath = "C:\\Users\\ajouk\\OneDrive\\Documents\\ideaProjects\\Restaurant_Management_System_With_Lujain\\Restaurant-Management-system-2024\\src\\main\\resources\\MenuItemsFile.txt";
+    private static final String typesAndIDs = "C:\\Users\\ajouk\\OneDrive\\Documents\\ideaProjects\\Restaurant_Management_System_With_Lujain\\Restaurant-Management-system-2024\\src\\main\\resources\\TypesAndIDsOfItems.txt";
+    private static final String accounts ="C:\\Users\\ajouk\\OneDrive\\Documents\\ideaProjects\\Restaurant_Management_System_With_Lujain\\Restaurant-Management-system-2024\\src\\main\\resources\\Accounts.txt";
 
     public static String getMenuItems() {
         return menuItemsFilePath;
