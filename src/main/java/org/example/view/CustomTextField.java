73f31c75f2c529cab6119bc179b2e61f8268e09a
Dana <dana.joukhadar3@gmail.com>
2025-01-19 03:20:17 +0300

    merging the code and hoping it works

--- src/main/java/org/example/view/CustomTextField.java
+++ src/main/java/org/example/view/CustomTextField.java
@@ -5,8 +5,9 @@ import java.awt.*;
 
 public class CustomTextField extends JTextField {
     String text;
+
     public CustomTextField() {
-        this.text=text;
+        this.text = text;
         this.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
         this.setForeground(new Color(215, 81, 132));
         this.setBackground(Color.WHITE);
@@ -18,22 +19,7 @@ public class CustomTextField extends JTextField {
 //        this.setHorizontalAlignment(JTextField.CENTER);
         this.setPreferredSize(new Dimension(200, 90));
     }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        if (getText().isEmpty()) {
-            Graphics2D g2 = (Graphics2D) g.create();
-            g2.setColor(new Color(215, 81, 132));
-            g2.setFont(new Font("Segoe UI Semibold", Font.ITALIC, 17));
-            g2.drawString(text, 5, 20);
-            g2.dispose();
-        }
-    }
 }
-
-        }
 
 //    @Override
 //    protected void paintComponent(Graphics g) {
@@ -47,4 +33,18 @@ public class CustomTextField extends JTextField {
 //            g2.dispose();
 //        }
 //    }
-}
+//}
+
+
+//    @Override
+//    protected void paintComponent(Graphics g) {
+//        super.paintComponent(g);
+//
+//        if (getText().isEmpty()) {
+//            Graphics2D g2 = (Graphics2D) g.create();
+//            g2.setColor(new Color(215, 81, 132));
+//            g2.setFont(new Font("Segoe UI Semibold", Font.ITALIC, 17));
+//            g2.drawString(text, 5, 20);
+//            g2.dispose();
+//        }
+//    }
